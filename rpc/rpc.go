6e361fd71524bb26f9a4511b3f94a9aad9828c63
adrianrazvanpetcu <apetcu@haventech.us>
2024-05-23 16:28:29 +0300

    decode

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -1,15 +1,42 @@
 package rpc
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"strconv"
 )
 
-func EncodeMessage(msg any) string{
+type BaseMessage struct {
+	Method string `json:"method"`
+}
+
+func EncodeMessage(msg any) string {
 	content, err := json.Marshal(msg)
 	if err != nil {
 		panic(err)
 	}
 
 	return fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(content), content)
+}
+
+func DecodeMessage(msg []byte) (string, []byte, error) {
+	header, content, found := bytes.Cut(msg, []byte{'\r', '\n', '\r', '\n'})
+	if !found {
+		return "", nil, errors.New("did not find separator")
+	}
+
+	contentLengthBytes := header[len("Content-Length: "):]
+	contentLength, err := strconv.Atoi(string(contentLengthBytes))
+	if err != nil {
+		return "", nil, err
+	}
+
+	var baseMessage BaseMessage
+	if err := json.Unmarshal(content[:contentLength], &baseMessage); err != nil {
+		return "", nil, err
+	}
+
+	return baseMessage.Method, content[:contentLength], nil
 }
--- rpc/rpc_test.go
+++ rpc/rpc_test.go
@@ -17,3 +17,19 @@ func TestEncode(t *testing.T) {
 		t.Fatalf("Expected %s, got %s", expected, actual)
 	}
 }
+
+func TestDecode(t *testing.T) {
+	incomingMessage := "Content-Length: 15\r\n\r\n{\"Method\":\"hi\"}"
+	method, content, err := rpc.DecodeMessage([]byte(incomingMessage))
+	contentLength:=len(content)
+	
+	if err != nil {
+		t.Fatal(err)
+	}
+	if contentLength != 15 {
+		t.Fatalf("Expected 15, got %d", contentLength)
+	}
+	if method != "hi" {
+		t.Fatalf("Expected hi, got %s", method)
+	}
+}
